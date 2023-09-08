c320a0982e1a7d89c4a6d2178a17fdb23f511f15
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-08 15:09:13 +0800

    Initial Commit

---
+++ Part8/Part8App.swift
@@ -0,0 +1,17 @@
+//
+//  Part8App.swift
+//  Part8
+//
+//  Created by Yuki Honda on 2023/09/08.
+//
+
+import SwiftUI
+
+@main
+struct Part8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
