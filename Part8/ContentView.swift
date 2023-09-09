66647d683c719400abdd2baab7d7867fb9a8b0b0
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-09 12:45:02 +0800

    first commit

--- Part8/ContentView.swift
+++ Part8/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("j土井ジェおfじょあいjフィジャ瀬fジョイj汐fじゃお, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
