10bce86a4d40e1e49adddb97bd5541837c355508
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-09 14:13:05 +0800

    リスト機能

--- Part8/ContentView.swift
+++ Part8/ContentView.swift
@@ -7,16 +7,27 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var taskData = [(title: "トマト", completed: false),
+                           (title: "ほうれん草", completed: false),
+                           (title: "卵", completed: false),
+                           (title: "パン", completed: false),
+                           (title: "コンソメ", completed: false),]
     var body: some View {
         NavigationStack{
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("j土井ジェおfじょあいjフィジャ瀬fジョイj汐fじゃお, world!")
+            List(0..<taskData.count, id: \.self) { index in
+                Button {
+                    taskData[index].completed.toggle()
+                } label: {
+                    HStack {
+                        Image(systemName: taskData[index].completed ? "checkmark.circle.fill" : "circle")
+                        Text(taskData[index].title)
+                    }
+                }
+                .foregroundColor(.primary)
             }
-            .padding()
+            .navigationTitle("ToDoリスト")
         }
     }
 }
